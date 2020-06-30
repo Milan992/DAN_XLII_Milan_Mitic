babf8247dce44c7d262d6c175538c1998cb61eed
Milan992 <mitic.gsm@gmail.com>
2020-06-30 22:42:20 +0200

    Created txt file for keeping possible locations for employees. Created a txt fil to log all the actions. Implemented a method to place all the locations from the Locations.txt file in a list.

--- DAN_XLII_Milan_Mitic/EmployeesApp/Service.cs
+++ DAN_XLII_Milan_Mitic/EmployeesApp/Service.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace EmployeesApp
 {
     class Service
     {
+        /// <summary>
+        /// Reads all lines from a txt file and returns a list of strings with all lines from the file.
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetAllLocations()
+        {
+            List<string> locations = new List<string>();
+            try
+            {
+                using (StreamReader sr = new StreamReader("../../Locations.txt"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        locations.Add(line);
+                    }
+                }
+            }
+            catch 
+            {
+                MessageBox.Show("There are no locations available");
+            }
+            return locations;
+        }
     }
 }
