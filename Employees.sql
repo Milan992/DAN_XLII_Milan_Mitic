38d4e7a1c20d0c1293aa54ee5926621dc8f3d8b2
Milan992 <mitic.gsm@gmail.com>
2020-06-30 14:45:14 +0200

    created and SQL script for creating the database of epmloyees

--- Employees.sql
+++ Employees.sql
@@ -3,27 +3,45 @@ CREATE DATABASE Employees
 GO
 USE Employees;
 
-if exists (SELECT name FROM sys.sysobjects WHERE name = 'tblOrder')
-drop table tblOrder;
+if exists (SELECT name FROM sys.sysobjects WHERE name = 'tblEmployee')
+drop table tblEmployee;
 
-if exists (SELECT name FROM sys.sysobjects WHERE name = 'tblMenu')
-drop table tblMenu;
+if exists (SELECT name FROM sys.sysobjects WHERE name = 'tblSector')
+drop table tblSector;
+
+if exists (SELECT name FROM sys.sysobjects WHERE name = 'tblGender')
+drop table tblGender;
 
 
-Create table tblMenu (
-MenuID int identity (1,1) primary key,
-Pizza int ,
-Chicken int,
-Sandwich int,
-Pasta int,
-Soup int 
+Create table tblGender (
+GenderID int identity (1,1) primary key,
+Gender varchar(10)
 )
 
-Create table tblOrder (
-OrderID int identity (1,1) primary key,
-MenuID int foreign key (MenuID) references tblMenu(MenuID) not null,
-OrderTime date,
-OrderNumber int
+Create table tblSector (
+SectorID int identity (1,1) primary key,
+SectorName varchar(10)
 )
 
+create table tblEmployee(
+EmployeeID int identity(1,1) primary key,
+FullName varchar(30) not null,
+DateOfBirth date not null,
+IdCardNumber char(9) check (LEN(IdCardNumber) = 9) not null unique,
+JMBG varchar(30) check(len(JMBG) = 13)  not null unique,
+GenderID int foreign key (GenderID) references tblGender(GenderID) not null,
+PhoneNumber int not null,
+SectorID int foreign key (SectorID) references tblSector(SectorID) not null,
+LocationOfEmployee varchar(30) not null,
+Manager int not null,
+constraint checkJMBG check(JMBG not like '%[^0-9]%'))
+
+insert into tblGender (Gender)
+values('Male');
+
+insert into tblGender (Gender)
+values('Female');
+
+insert into tblGender (Gender)
+values('Other');
 
